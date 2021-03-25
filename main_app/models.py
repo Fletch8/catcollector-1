a614160d794c55f8cefc8c5b434813ffd70f72d2
James Sinkler <jamessinkler713@gmail.com>
2021-03-25 10:50:57 -0700

    DjangoAuth: 1/2 way

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+# thank you Django for this user :-)
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Cat(models.Model):
@@ -6,6 +10,8 @@ class Cat(models.Model):
     breed = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     name = models.CharField(max_length=100)
+    # this is associated with a user
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
